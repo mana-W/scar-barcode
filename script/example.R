5ce9fe4edc31555fa4f6e0f6714611abc50864a2
mana-W <83333092+mana-W@users.noreply.github.com>
2021-11-17 22:00:48 +0800

    Create example.R

---
+++ script/example.R
@@ -0,0 +1,20 @@
+#example
+#data is from UMI_CB/CB_UMI
+#fa is red file
+#cutsite is a file define each sgRNA start and end positon
+
+library(Slin)
+
+data<-read.table("UMI_CB/CB_UMI",sep="\t",header=T)
+ref<-ReadFasta("V3.fasta")
+cutsite<-read.table("V3.cutSites",col.names = c("indx","start","end"))
+scarref<-ReadCutsite(cutsite)
+scarref_all<-ReadCutsite(cutsite,reftype="ALL")
+
+#array identify
+scarinfo<-FindScar(data=data,scarfull=ref,scar=cutsite,indel.coverage="ALL",type="test",cln=8)
+scarform<-INDELChangeForm(scarinfo,scarref = scarref_all,cln=4)
+
+scarident
+
+#pattern visualization 
